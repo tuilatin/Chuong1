bdcfd56d026160d1e9f3630a7479dfe33ca42a49
tuilatin <167632727+tuilatin@users.noreply.github.com>
2025-02-21 20:34:50 +0700

    Add files via upload
    code bài 5 và main

---
+++ Bai5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuong1
+{
+    class Bai5
+    {
+        public static void Run()
+        {
+            Console.Write("Nhập số nguyên dương n: ");
+            int n_144;
+            while (!int.TryParse(Console.ReadLine(), out n_144) || n_144 <= 0)
+            {
+                Console.Write("Vui lòng nhập số nguyên dương: ");
+            }
+
+            int count_144 = 0;
+            int temp_144 = n_144;
+
+            while (temp_144 > 0)
+            {
+                temp_144 /= 10; // Loại bỏ chữ số cuối cùng
+                count_144++; // Tăng biến đếm 
+            }
+            Console.WriteLine($"Số chữ số của {n_144} là: {count_144}");
+        }
+    }
+}
