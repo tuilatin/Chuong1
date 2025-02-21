f2e7c8f9787ad8fdc1af8a675a688091b76a5822
tuilatin <167632727+tuilatin@users.noreply.github.com>
2025-02-21 20:30:46 +0700

    Add files via upload
    Code bài 2

---
+++ Bai2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Chuong1
+{
+    class Bai2
+    {
+        public static void Run()
+        {
+            Console.Write("nhập n: ");
+            int n_144;
+            while (!int.TryParse(Console.ReadLine(), out n_144) || n_144 <= 0)
+            {
+                Console.Write("Vui lòng nhập số nguyên dương: ");
+            }
+            long sum_144 = 0;
+            long factorial_144 = 1;
+            for (int i_144 = 1; i_144 <= n_144; i_144++)
+            {
+                factorial_144 *= i_144; // Tính giai thừa
+                sum_144 += factorial_144; // Cộng vào tổng.
+            }
+
+            Console.WriteLine($"S({n_144}) = {sum_144}");
+        }
+    }
+}
