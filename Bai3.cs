4824241af3313be32460f46d50fe5e1a05991f65
tuilatin <167632727+tuilatin@users.noreply.github.com>
2025-02-21 20:33:46 +0700

    Add files via upload
    Code bài 3 và main

---
+++ Bai3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuong1
+{
+    class Bai3
+    {
+        public static void Run()
+        {
+            Console.Write("Nhập số nguyên dương n: ");
+            int n_144;
+            while (!int.TryParse(Console.ReadLine(), out n_144) || n_144 <= 0)
+            {
+                Console.Write("Vui lòng nhập số nguyên dương: ");
+            }
+
+            int sum_144 = 0;
+
+            for (int i_144 = 1; i_144 <= n_144; i_144++)
+            {
+                if (n_144 % i_144 == 0) // Kiểm tra i_144 có phải ước của n_144 không.
+                {
+                    sum_144 += i_144;
+                }
+            }
+            Console.WriteLine($"Tổng các ước của {n_144} là: {sum_144}");
+        }
+    }
+}
